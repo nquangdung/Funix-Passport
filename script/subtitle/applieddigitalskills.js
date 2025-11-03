348fcbceb044a868e50dd6e5e45e3622890878ba
= <=>
2025-11-03 18:58:16 +0700

    Revert "Format file"
    This reverts commit e605880d32239b0e2e883c5bc4497dfdc5a0e5e4.

--- script/subtitle/applieddigitalskills.js
+++ script/subtitle/applieddigitalskills.js
@@ -1,95 +1,92 @@
 class Applieddigitalskills {
-	constructor() {}
+    constructor() {
+    }
 
-	run() {
-		this.initComponent();
-	}
+    run() {
+        this.initComponent();
+    }
 
-	initComponent() {
-		this.initData().then((status) => {
-			if (status) {
-				getSettingData().then((res) => {
-					let subtitleMode = res.modeSubtitle;
-					if (subtitleMode === '0') {
-						Notifycation.confirmSubtitle().then((mode) => {
-							if (mode !== 0) {
-								this.createElement(mode);
-							}
-						});
-					}
-					// else if (subtitleMode === "0") {
-					//    this.createElement(1);
-					// } else if (subtitleMode === "2") {}
-				});
-			}
-		});
-	}
+    initComponent() {
+        this.initData().then((status) => {
+            if (status) {
+                getSettingData().then(res => {
+                    let subtitleMode = res.modeSubtitle;
+                    if (subtitleMode === "0") {
+                        Notifycation.confirmSubtitle().then(mode => {
+                            if (mode !== 0) {
+                                this.createElement(mode);
+                            }
+                        });
+                    }
+                    // else if (subtitleMode === "0") {
+                    //    this.createElement(1);
+                    // } else if (subtitleMode === "2") {}
+                })
+            }
+        });
+    }
 
-	createElement(mode) {
-		// Add subtitle
-		const html =
-			'<div id="funix-subtitle" style=" position: absolute; bottom: 12%; left: 10%; background-color: rgba(0, 0, 0, 0.75); height: 7%; width: 80%; text-align: center; "> <p style=" color:white; font-size: 1vw; left: 50%; margin: 0; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); "></p> </div>';
-		let container = $(html);
-		let subtitle = container.find('p')[0];
-		this.subtitleObserver = new subtitleObserver('#funix-subtitle > p');
-		this.subtitleObserver.initData(this.vi, this.en);
-		this.subtitleObserver.mode = mode;
+    createElement(mode) {
+        // Add subtitle
+        const html = '<div id="funix-subtitle" style=" position: absolute; bottom: 12%; left: 10%; background-color: rgba(0, 0, 0, 0.75); height: 7%; width: 80%; text-align: center; "> <p style=" color:white; font-size: 1vw; left: 50%; margin: 0; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); "></p> </div>';
+        let container = $(html);
+        let subtitle = container.find("p")[0];
+        this.subtitleObserver = new subtitleObserver("#funix-subtitle > p");
+        this.subtitleObserver.initData(this.vi, this.en);
+        this.subtitleObserver.mode = mode;
 
-		$('.student-video').append(container);
-		$('video > track').remove();
+        $(".student-video").append(container);
+        $("video > track").remove();
 
-		this.startObserver();
-	}
+        this.startObserver();
+    }
 
-	startObserver() {
-		let video = $('video').get(0);
-		if (video !== undefined) {
-			this.subtitleObserver.startObserver(video);
-		} else {
-			setTimeout(function () {
-				startObserver();
-			}, 1000);
-		}
-	}
+    startObserver() {
+        let video = $("video").get(0);
+        if (video !== undefined) {
+            this.subtitleObserver.startObserver(video);
+        } else {
+            setTimeout(function () {
+                startObserver();
+            }, 1000);
+        }
+    }
 
-	async initData() {
-		// Get id video
-		let id = window.location.pathname
-			.split('/')
-			.splice(-1)[0]
-			.replace('.html', '');
-		let request = {
-			content: 'POST Request',
-			requestUrl: 'https://funix-subtitle.firebaseapp.com/get',
-			requestBody: {
-				cid: 'applieddigitalskills',
-				lid: id,
-			},
-		};
-		let resAPI = {};
-		await sendMessagePromise(request).then((res) => {
-			resAPI = res;
-		});
-		if (resAPI.code === 200) {
-			let self = this;
-			await sendMessagePromise({
-				content: 'GET Request',
-				requestUrl: resAPI.data.vi,
-			}).then((data) => {
-				self.vi = SubtitleHandling.parseSub(data);
-			});
-			await sendMessagePromise({
-				content: 'GET Request',
-				requestUrl: resAPI.data.en,
-			}).then((data) => {
-				self.en = SubtitleHandling.parseSub(data);
-			});
-			return true;
-		}
-		return false;
-	}
+    async initData() {
+        // Get id video
+        let id = window.location.pathname.split("/").splice(-1)[0].replace(".html", "");
+        let request = {
+            content: "POST Request",
+            requestUrl: "https://funix-subtitle.firebaseapp.com/get",
+            requestBody: {
+                cid: "applieddigitalskills",
+                lid: id
+            }
+        };
+        let resAPI = {};
+        await sendMessagePromise(request).then(res => {
+            resAPI = res;
+        });
+        if (resAPI.code === 200) {
+            let self = this;
+            await sendMessagePromise({
+                content: "GET Request",
+                requestUrl: resAPI.data.vi,
+            }).then(data => {
+                self.vi = SubtitleHandling.parseSub(data);
+            });
+            await sendMessagePromise({
+                content: "GET Request",
+                requestUrl: resAPI.data.en,
+            }).then(data => {
+                self.en = SubtitleHandling.parseSub(data);
+            });
+            return true;
+        }
+        return false;
+    }
 }
 
 $(document).ready(function () {
-	new Applieddigitalskills().run();
+    (new Applieddigitalskills()).run();
 });
