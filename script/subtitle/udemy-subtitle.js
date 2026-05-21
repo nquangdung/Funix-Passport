3829d327ed03f461af3a8d1fcca2a83ca4dfa188
minhnnfx18096 <minhnnfx18096@funix.edu.vn>
2026-05-22 03:34:38 +0700

    Fix: Delete all console.log

--- script/subtitle/udemy-subtitle.js
+++ script/subtitle/udemy-subtitle.js
@@ -20,8 +20,6 @@ const caption = '[class^="captions-display--captions-container"]',
 	direct_sub_node = '#funixSubtitle';
 
 async function initData() {
-	console.log(9 + '-udemy-subtitle');
-
 	vi = [];
 	eng = [];
 	jp = [];
@@ -116,7 +114,6 @@ async function initData() {
 }
 
 $(document).ready(function () {
-	console.log(1 + '-udemy-subtitle');
 
 	enable = false;
 	udemySubtitleObserver = new subtitleObserver(direct_sub_node); //Observe the paragraph
@@ -130,7 +127,6 @@ $(document).ready(function () {
 });
 
 function initComponents() {
-	console.log(2 + '-udemy-subtitle');
 	// Setup Subtitle button
 	//initButton();----remove
 
@@ -148,7 +144,6 @@ function initComponents() {
 }
 
 function pageLoad(code) {
-	console.log(3 + '-udemy-subtitle');
 	startObserver();
 	if (code === 200) {
 		getSettingData().then((res) => {
@@ -166,7 +161,6 @@ function pageLoad(code) {
 }
 
 function start(type, float) {
-	console.log(4 + '-udemy-subtitle');
 
 	udemySubtitleObserver.mode = type;
 
@@ -178,7 +172,6 @@ function start(type, float) {
 }
 
 function startObserver() {
-	console.log(5 + '-udemy-subtitle');
 
 	let video = $('video').get(0);
 	if (video === undefined) {
@@ -202,7 +195,6 @@ function startObserver() {
 }
 
 function initSubnode(mode) {
-	console.log(6 + '-udemy-subtitle');
 
 	const subtitleObject = $(
 		'<div class="captions-display--captions-container--1-aQJ"> <div class="captions-display--captions-cue-text--ECkJu" data-purpose="captions-cue-text" style="font-size: 26.36px; opacity: 0.75;justify-content: center;text-align: center;" id="funixSubtitle"></div> </div>'
@@ -225,7 +217,6 @@ function initSubnode(mode) {
 }
 
 function initButton() {
-	console.log(7 + '-udemy-subtitle');
 
 	// Init elements
 	button = $(
@@ -292,7 +283,6 @@ function initButton() {
 }
 
 function setActiveButton(buttonActive) {
-	console.log(8 + '-udemy-subtitle');
 
 	viBtn.removeClass('active');
 	engBtn.removeClass('active');
